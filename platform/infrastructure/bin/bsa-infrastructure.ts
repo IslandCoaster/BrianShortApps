4539a35b7ad7668c25c49be1c313525c7b53d65e
IslandCoaster <short.brians@gmail.com>
2026-07-17 17:15:33 -0400

    PLAT-001.1: establish infrastructure workspace

---
+++ platform/infrastructure/bin/bsa-infrastructure.ts
@@ -0,0 +1,9 @@
+﻿import { App } from "aws-cdk-lib";
+
+import { PlatformFoundationStack } from "../stacks/PlatformFoundationStack";
+
+const app = new App();
+
+new PlatformFoundationStack(app, "bsa-platform-foundation-dev", {
+  description: "BrianShortApps development cloud foundation.",
+});
---
+++ platform/infrastructure/stacks/PlatformFoundationStack.ts
@@ -0,0 +1,12 @@
+﻿import { Stack, type StackProps } from "aws-cdk-lib";
+import type { Construct } from "constructs";
+
+export class PlatformFoundationStack extends Stack {
+  public constructor(
+    scope: Construct,
+    id: string,
+    props?: StackProps,
+  ) {
+    super(scope, id, props);
+  }
+}
